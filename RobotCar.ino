73b058d31b065ebdf2f84ad592f8d77632617960
Andy Prevalsky <andyprevalsky@yahoo.com>
2017-12-04 18:48:10 -0600

    initial RObot

---
+++ RobotCar.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(P2_2, OUTPUT)
+  pinMode(P3_0, OUTPUT)
+  pinMode(P3_3, OUTPUT)
+  pinMode(P6_6, OUTPUT)
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(P2_2, HIGH)
+   digitalWrite(P3_3, HIGH)
+   digitalWrite(P3_0, LOW)
+   digitalWrite(P6_6, LOW)
+  // put your main code here, to run repeatedly: 
+  
+}
