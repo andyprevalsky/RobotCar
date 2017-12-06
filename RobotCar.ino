a7f78c03539a5272e298cc02ad1908690f4b7b3b
Codetron <balakumaran55@gmail.com>
2017-12-05 22:54:02 -0600

    added photoresistor dependency

--- RobotCar.ino
+++ RobotCar.ino
@@ -1,6 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
-  // Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
@@ -9,8 +9,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH); 
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  delay(100);
+  Serial.println(sensorValue);
+
+  if (sensorValue < 120) {
+    digitalWrite(4, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(7, LOW);
+  } else {
+    digitalWrite(4, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(7, LOW);
+  }
 }
