5444c71fe1d6cde1ea47d8e7332cf2f2c64cab5c
Andy Prevalsky <andyprevalsky@yahoo.com>
2017-12-06 22:55:04 -0600

    updated turns

--- RobotCar.ino
+++ RobotCar.ino
@@ -12,19 +12,34 @@ void setup() {
 void loop() {
   //float voltage = sensorValue * (5.0 / 1023.0);
   // put your main code here, to run repeatedly:
-  int RigthSensorBefore = analogRead(A0);
+  int RigthPSensorBefore = analogRead(A0);
+  int LeftPSensorBefore = analogRead(A1); //XX is pin connected to other photo resistor
   delay(100);
-  int RightSensorAfter = analogRead(A0);
+  int LeftPSensorAfter = analogRead(A0);
+  int RightPSensorAfter = analogRead(A1);
 
-  if (RightSensorAfter + 30 < RigthSensorBefore) {
+  if (RightPSensorAfter + 30 < RigthPSensorBefore) {
+    digitalWrite(4, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(7, LOW);
+    delay(900);
+  } else {
     digitalWrite(4, HIGH);
     digitalWrite(6, HIGH);
     digitalWrite(5, LOW);
     digitalWrite(7, LOW);
+  }
+
+  if (LeftPSensorAfter + 30 < LeftPSensorBefore) {
+    digitalWrite(4, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(7, HIGH);
     delay(900);
   } else {
-    digitalWrite(4, LOW);
-    digitalWrite(6, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(5, LOW);
     digitalWrite(7, LOW);
   }
