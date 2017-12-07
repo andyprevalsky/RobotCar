90f2938fb648ff71a985b1509b581aa6e765fece
Andy Prevalsky <andyprevalsky@yahoo.com>
2017-12-06 19:57:45 -0600

    better

--- RobotCar.ino
+++ RobotCar.ino
@@ -1,3 +1,5 @@
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,23 +10,22 @@ void setup() {
 }
 
 void loop() {
+  int list[];
   // put your main code here, to run repeatedly:
   int sensorValue = analogRead(A0);
   float voltage = sensorValue * (5.0 / 1023.0);
   delay(100)
   int sensorValue1 = analogRead(A0);
 
-  if (sensorValue + 30 < sensorValue1) {
+  if (sensorValue1 + 30 < sensorValue) {
     digitalWrite(4, HIGH);
     digitalWrite(6, HIGH);
     digitalWrite(5, LOW);
     digitalWrite(7, LOW);
+    delay(1000) 
   } else {
     digitalWrite(4, LOW);
     digitalWrite(6, LOW);
     digitalWrite(5, LOW);
     digitalWrite(7, LOW);
   }
-  
-  
-}
